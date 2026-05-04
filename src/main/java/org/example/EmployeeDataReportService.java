770dc49ae5f5f626f466f2a38c778be2c42f6062
SREELAKSHMI AJITHAN <sreelakshmi.ajithan@mercedes-benz.com>
2026-05-04 16:40:38 +0530

    code clean up and issue fix

--- src/main/java/org/example/EmployeeDataReportService.java
+++ src/main/java/org/example/EmployeeDataReportService.java
@@ -6,33 +6,30 @@ import java.util.*;
 
 public class EmployeeDataReportService {
     public void employeeReportGeneration() {
-	//Can have up to 1000 employees - done
-	//employee details in csv file - done
-	//Manager earns >= 20%, <=50% of the average salary of direct suboordinates - done
-	//max employee + 4 levels = ceo
-	//which managers earns less than they should and by how much - done
-	//which managers earns more than they should and by how much - done
-	//which managers have reporting line more than 4 and by how much
 
-	Path employeeDataFile = Path.of("C:/Users/sajitha/Documents/Test/src/main/resources/testfile.csv");
-	Map<Long, EmployeeDTO> employeeIdEmployeeDTOMap = new HashMap<>();
-	Map<Long, List<EmployeeDTO>> managerReportingEmployeeMap = fetchEmployeeDataFromCSVFile(employeeDataFile,
-		employeeIdEmployeeDTOMap);
-	if (!managerReportingEmployeeMap.isEmpty()) {
-	    managerSalaryCheck(managerReportingEmployeeMap, employeeIdEmployeeDTOMap);
+	try {
+	    Path employeeDataFile = Path.of(
+		    Objects.requireNonNull(getClass().getClassLoader().getResource("testfile.csv")).toURI());
+	    Map<Long, EmployeeDTO> employeeIdEmployeeDTOMap = new HashMap<>();
+	    Map<Long, List<EmployeeDTO>> managerReportingEmployeeMap = fetchEmployeeDataFromCSVFile(employeeDataFile,
+		    employeeIdEmployeeDTOMap);
+	    if (!managerReportingEmployeeMap.isEmpty()) {
+		employeeReportGeneration(managerReportingEmployeeMap, employeeIdEmployeeDTOMap);
+	    }
+	} catch (Exception e) {
+	    System.out.println("Failed to read the file,{}" + e.getMessage());
 	}
 
     }
 
-    private void managerSalaryCheck(Map<Long, List<EmployeeDTO>> managerReportingEmployeeMap,
+    private void employeeReportGeneration(Map<Long, List<EmployeeDTO>> managerReportingEmployeeMap,
 	    Map<Long, EmployeeDTO> employeeIdEmployeeDTOMap) {
 	for (Map.Entry<Long, List<EmployeeDTO>> entry : managerReportingEmployeeMap.entrySet()) {
 	    Long managerId = entry.getKey();
 	    if (managerId == null)
 		continue;
-	    checkTheReportingLineMoreThanFour(entry, managerReportingEmployeeMap,employeeIdEmployeeDTOMap);
 	    checkSalaryRangeOfManagers(managerId, entry.getValue(), employeeIdEmployeeDTOMap);
-
+	    checkTheReportingLineMoreThanFour(entry, managerReportingEmployeeMap, employeeIdEmployeeDTOMap);
 	}
 
     }
@@ -41,30 +38,25 @@ public class EmployeeDataReportService {
 	    Map<Long, List<EmployeeDTO>> managerReportingEmployeeMap, Map<Long, EmployeeDTO> employeeIdEmployeeDTOMap) {
 	Map<Long, Integer> employeeReportingLineCount = new HashMap<>();
 	Set<Long> employeeCheck = new HashSet<>();
-	int depth = reportingLineCountCheck(entry.getKey(), managerReportingEmployeeMap, employeeReportingLineCount, employeeCheck);
+	int depth = reportingLineCountCheck(entry.getKey(), managerReportingEmployeeMap, employeeReportingLineCount,
+		employeeCheck);
 	EmployeeDTO manager = employeeIdEmployeeDTOMap.get(entry.getKey());
 	if (depth > 4) {
 	    int excess = depth - 4;
-	    System.out.printf(
-		    "Manager %s %s has reporting depth %d (>4 by %d)%n",
-		     manager.getFirstName(), manager.getLastName(), depth, excess
-	    );
+	    System.out.printf("Manager %s %s has reporting depth %d (>4 by %d)%n", manager.getFirstName(),
+		    manager.getLastName(), depth, excess);
 	}
 
     }
 
-
-    private int reportingLineCountCheck(Long managerId,
-	    Map<Long, List<EmployeeDTO>> directsByManager,
-	    Map<Long, Integer> employeeReportingLineCount,
-	    Set<Long> employeeCheck) {
+    private int reportingLineCountCheck(Long managerId, Map<Long, List<EmployeeDTO>> directsByManager,
+	    Map<Long, Integer> employeeReportingLineCount, Set<Long> employeeCheck) {
 
 	if (employeeReportingLineCount.containsKey(managerId)) {
 	    return employeeReportingLineCount.get(managerId);
 	}
 
 	if (!employeeCheck.add(managerId)) {
-	    // Cycle detected (bad data): treat as 0 to avoid infinite loop
 	    return 0;
 	}
 
@@ -72,7 +64,8 @@ public class EmployeeDataReportService {
 	int maxChildDepth = 0;
 
 	for (EmployeeDTO e : directs) {
-	    int childDepth = reportingLineCountCheck(e.getId(), directsByManager, employeeReportingLineCount, employeeCheck);
+	    int childDepth = reportingLineCountCheck(e.getId(), directsByManager, employeeReportingLineCount,
+		    employeeCheck);
 	    maxChildDepth = Math.max(maxChildDepth, childDepth);
 	}
 
@@ -85,23 +78,30 @@ public class EmployeeDataReportService {
 
     private void checkSalaryRangeOfManagers(Long managerId, List<EmployeeDTO> employeeDTOList,
 	    Map<Long, EmployeeDTO> employeeIdEmployeeDTOMap) {
-	Double averageSalary = averageSalaryOfDirectSuboordinates(employeeDTOList);
-	Double managerSalary = Double.valueOf(employeeIdEmployeeDTOMap.get(managerId).getSalary());
-	Double percentageDiff = fetchSalaryPercentageDifference(managerSalary, averageSalary);
 	String managerFirstName = employeeIdEmployeeDTOMap.get(managerId).getFirstName();
 	String managerLastName = employeeIdEmployeeDTOMap.get(managerId).getLastName();
+
+	Double averageSalary = averageSalaryOfDirectSuboordinates(employeeDTOList);
+	Double managerSalary = Double.valueOf(employeeIdEmployeeDTOMap.get(managerId).getSalary());
+	Double percentageDiff = computePercentageDifference(managerSalary, averageSalary);
+	Double salaryMargin = 0.0;
 	if (percentageDiff < 20) {
-	    System.out.printf("Manager %s %s earns less than they should by %.2f %n.", managerFirstName,
-		    managerLastName, averageSalary - managerSalary);
+	    salaryMargin = averageSalary * 20 / 100;
+	    System.out.printf("Manager %s %s earns less than they should by %.2f. %n", managerFirstName,
+		    managerLastName, Math.max(managerSalary, averageSalary + salaryMargin) - Math.min(managerSalary,
+			    managerSalary + salaryMargin));
 	} else if (percentageDiff > 50) {
-	    System.out.printf("Manager %s %s earns less than they should by %.2f %n.", managerFirstName,
-		    managerLastName, managerSalary - averageSalary);
+	    salaryMargin = averageSalary * 50 / 100;
+	    System.out.printf("Manager %s %s earns more than they should by %.2f. %n", managerFirstName,
+		    managerLastName,
+		    Math.max(salaryMargin + averageSalary, managerSalary) - Math.min(salaryMargin + averageSalary,
+			    managerSalary));
 	}
     }
 
-    private Double fetchSalaryPercentageDifference(Double managerSalary, Double averageSalary) {
-	Double value = Math.max(managerSalary, averageSalary) - Math.min(managerSalary, averageSalary);
-	return (value / averageSalary) * 100;
+    private Double computePercentageDifference(Double managerSalary, Double averageSalary) {
+	Double salaryDiff = Math.max(managerSalary, averageSalary) - Math.min(managerSalary, averageSalary);
+	return (salaryDiff / averageSalary) * 100;
     }
 
     private Double averageSalaryOfDirectSuboordinates(List<EmployeeDTO> employeeDTOList) {
@@ -113,12 +113,16 @@ public class EmployeeDataReportService {
     private Map<Long, List<EmployeeDTO>> fetchEmployeeDataFromCSVFile(Path employeeDataFile,
 	    Map<Long, EmployeeDTO> employeeIdEmployeeDTOMap) {
 	Map<Long, List<EmployeeDTO>> managerReportingEmployeeMap = new HashMap<>();
+	//Reading excel file and creating employeeDTO and manager reporting employee map
 	try (var rows = Files.lines(employeeDataFile)) {
 	    rows.skip(1).map(row -> row.split(",")).forEach(col -> {
-		List<EmployeeDTO> reportingEmpIds = new ArrayList<>();
+
 		EmployeeDTO employeeDTO = new EmployeeDTO(Long.valueOf(col[0]), col[1], col[2], Long.valueOf(col[3]),
 			(col.length > 4) ? Long.parseLong(col[4]) : null);
+
 		employeeIdEmployeeDTOMap.put(employeeDTO.getId(), employeeDTO);
+
+		List<EmployeeDTO> reportingEmpIds = new ArrayList<>();
 		if (managerReportingEmployeeMap.containsKey(employeeDTO.getManagerId())) {
 		    reportingEmpIds = managerReportingEmployeeMap.get(employeeDTO.getManagerId());
 		    reportingEmpIds.add(employeeDTO);
@@ -127,11 +131,9 @@ public class EmployeeDataReportService {
 		    reportingEmpIds.add(employeeDTO);
 		    managerReportingEmployeeMap.put(employeeDTO.getManagerId(), reportingEmpIds);
 		}
-		System.out.println(employeeDTO.getId() + " -> " + employeeDTO.getFirstName());
 	    });
 
 	} catch (Exception e) {
-	    //	    e.printStackTrace();
 	    System.out.println("Failed to read the file,{}" + e.getMessage());
 	}
 	return managerReportingEmployeeMap;
